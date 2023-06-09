caea350683c7989373fca003b78e61bac250269e
liotfra <134953844+liotfra@users.noreply.github.com>
2023-06-09 13:09:12 +0000

    message commit

--- demo/src/test/java/com/example/AppTest.java
+++ demo/src/test/java/com/example/AppTest.java
@@ -1,8 +1,7 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
-
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
