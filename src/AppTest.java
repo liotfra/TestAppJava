4a36922bad809ac5ee3a952b7add1be71ee16c75
liotfra <134953844+liotfra@users.noreply.github.com>
2023-06-09 08:34:46 +0000

    New files

---
+++ src/AppTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.assertTrue;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void test1true() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void test2true() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testsum() {
+        assertEquals(null, 2, 1 + 2, 0);;
+    }
+}
